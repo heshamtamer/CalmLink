94c9e8ab4b01d7ba4844f063cdd4b259d895ba7e
heshamtamer <hesham1762002@yahoo.com>
2025-04-24 21:43:27 +0200

    yarb

--- server/middleware/validateTokenHandler.js
+++ server/middleware/validateTokenHandler.js
@@ -7,6 +7,7 @@ const validateToken = asyncHandler(async (req, res, next) => {
 
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     token = req.headers.authorization.split(' ')[1]; // Get the token from the header
+    console.log('Token1:', token); // Log the token for debugging
   }
 
   if (!token) {
@@ -16,6 +17,7 @@ const validateToken = asyncHandler(async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    console.log('Decoded:', decoded); // Log the decoded token for debugging
     req.user = await User.findById(decoded.user.id).select('-password'); // Get user without password
     next();
   } catch (error) {
